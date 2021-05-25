8c29f4f769db7e78088a00cc63293348401e865e
David Vasek <david.vasek51@gmail.com>
2021-05-25 15:48:05 -0500

    end of day push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const myNameInput = document.querySelector('#myNameInput');
+const favoriteCandyInput = document.querySelector('#favoriteCandyInput')
+const submitButton = document.querySelector('#submitButton');
+const submitButton1 = document.querySelector('#submitButton1');
+
+function 
+
+
+submitButton.addEventListener('click', function () {
+    console.log(myNameInput.value);
+})
+
+submitButton1.addEventListener('click', function () {
+    console.log(favoriteCandyInput.value);
+})
